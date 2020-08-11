2cb302e3721fb8951158df65cc1e1ac16eba1d35
Nicholas Mullen <nwmullen@gmail.com>
2020-08-10 22:00:52 -0400

    single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,16 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    existence_hash = {}
+    for n in arr:
+        if n in existence_hash:
+            existence_hash.pop(n)
+        else:
+            existence_hash[n] = 1
+    return list(existence_hash.keys())[0]
 
 
 if __name__ == '__main__':
@@ -13,3 +19,8 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
+
+# Doing this in O(1)?
+# Use the array object as the memo?
+# Delete elements from the array as I put them into the dictionary?
